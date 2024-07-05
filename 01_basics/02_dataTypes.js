ba1f4ab37f90fe572272ad43a64f91b1d16061da
Anaghjaiswar <168277774+Anaghjaiswar@users.noreply.github.com>
2024-07-05 16:42:40 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) //we are using node.js not browser
+
+console.log(3 + 3)
+console.log("Anagh")
+
+let name = "anagh"
+let age = 20
+let isloggedIn = false
+let state;
+
+
+//Data-types(primitives)
+/*
+number => 2^52 (range)
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
+console.log(typeof "Anagh");
+console.log(typeof 20);
+console.log(typeof false);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
