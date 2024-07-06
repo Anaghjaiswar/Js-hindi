75738e1d5d608d1209978efa69fa4c0c9908f218
Anaghjaiswar <168277774+Anaghjaiswar@users.noreply.github.com>
2024-07-06 11:18:26 +0000

    operators and operands

--- 01_basics/03_conversionoperators.js
+++ 01_basics/03_conversionoperators.js
@@ -1,11 +1,11 @@
 let score = "54y6twrhwdty"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 /*
 "33" => 33
@@ -16,14 +16,52 @@ true => 1; false => 0
 let isloggedin = 1
 
 let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin);
+//console.log(booleanisloggedin);
 
 // 1 => true ; 0 => false
 // "" => false
 // "hitesh" => false
 
 let somenumber = 33
-
 let stringnumber = String(somenumber);
-console.log(somenumber);
-console.log(typeof stringnumber);
+// console.log(somenumber);
+// console.log(typeof stringnumber);
+
+// ***************************operation ************************
+
+let value = 3
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(3**2);
+// console.log(9%2);
+
+let str1 ="hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+//console.log(true+);// error
+console.log(+"");
+
+
+let num1,num2,num3
+
+num1= num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
