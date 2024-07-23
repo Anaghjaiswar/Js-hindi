037f802f0a31870931799a5601001cce03f620c8
Anaghjaiswar <168277774+Anaghjaiswar@users.noreply.github.com>
2024-07-23 22:26:44 +0000

    arrrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// array
+
+const myArr = [0,1,2,3,4,5]//ek arrray me similar and non similiar typeof data dono hi sakta hai
+//array indexing starts from zero
+
+const myHeroes = ["ironman", "batman", "superman"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr[0]);
+console.log(myArr[1].length);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()// last value ko remove kar dega
+
+myArr.unshift(9);// 9 insert hua hai array ki shuruat me
+
+console.log(myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
