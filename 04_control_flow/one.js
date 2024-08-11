cbfff46d783e30c2492d24237efbcbd36ea7e598
Anaghjaiswar <168277774+Anaghjaiswar@users.noreply.github.com>
2024-08-11 06:23:36 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,112 @@
+//if
+
+const isUserLoggedIn = true
+const temperature = 41
+
+// if(condition/*this should be true*/){
+
+// }
+
+// if (temperature===41){
+//     console.log("less than 50");
+
+// }else{
+//     console.log("temperature is grater than 50")
+// }
+
+
+
+// operators
+/*
+<(less than)
+>(greater than)
+<=(less than or equal to)
+>=(greater than equal to)
+==(to check if both are equal)
+!=(to check if both are not equal)
+===(it also checks data type and value)//strict checking
+!==(they are not equal in both value and type)// strict inequality operator
+
+*/
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User Power :${power}`);
+
+// }
+// console.log(`User Power :${power}`);
+
+//shorthand notation
+
+const balance = 1000
+
+//if (balance > 500) console.log("test"),console.log("test2"); // halake aise likhna nahi hai
+// if (balance < 500) {
+//     console.log("less than");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+    
+// }
+
+
+const userLoggedin = true
+const debitCard = true
+const loggedInFormGoogle = false
+const LoggedInFromEmail = true
+if(userLoggedin && debitCard && 2==2){
+    console.log("allow to buy Courses");
+    
+}
+if(loggedInFormGoogle || LoggedInFromEmail){
+    console.log("User Logged In");
+    
+}
+
+
+//&& - isme sari condition true honi chahiye
+//|| - isme koi bhi ek condition true honi chahiye
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
